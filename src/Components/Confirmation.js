e418ce80a8cd4134f0a9102265cae85152d75a05
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-10-09 22:00:18 -0400

    created Route Links

--- src/Components/Confirmation.js
+++ src/Components/Confirmation.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Confirmation = () => {
     return (
         <div>
-            
+            Confirmed
         </div>
     )
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 const Form = () => {
+
     return (
         <div>
-            
+            Form
         </div>
     )
 }
